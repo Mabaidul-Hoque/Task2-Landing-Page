b63272881be6e78fb96d38cd1079b3055616aefc
MABAIDUL HOQUE <132288979+Mabaidul-Hoque@users.noreply.github.com>
2024-03-26 16:58:28 +0530

    updated whole ui with responsiveness

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -5,36 +5,16 @@ const ImageCarousel = () => {
     <>
       <Carousel autoplay className="pl-0 lg:pl-[10%] xl:pl-28 py-10">
         <div className="text-center flex items-center justify-center">
-          {/* <h3 style={contentStyle}>1</h3> */}
-          <img
-            // className="w-2/3 md:w-full"
-            src="/demo-product-showcase-product-01.png"
-            alt=""
-          />
+          <img src="/demo-product-showcase-product-01.png" alt="" />
         </div>
         <div>
-          {/* <h3 style={contentStyle}>2</h3> */}
-          <img
-            // className="w-2/3 md:w-full"
-            src="/demo-product-showcase-product-02.webp"
-            alt=""
-          />
+          <img src="/demo-product-showcase-product-02.webp" alt="" />
         </div>
         <div>
-          {/* <h3 style={contentStyle}>3</h3> */}
-          <img
-            // className="w-2/3 md:w-full"
-            src="/demo-product-showcase-product-03.png"
-            alt=""
-          />
+          <img src="/demo-product-showcase-product-03.png" alt="" />
         </div>
         <div>
-          {/* <h3 style={contentStyle}>4</h3> */}
-          <img
-            // className="w-2/3 md:w-full"
-            src="/demo-product-showcase-product-04.png"
-            alt=""
-          />
+          <img src="/demo-product-showcase-product-04.png" alt="" />
         </div>
       </Carousel>
     </>
--- src/components/SlidingImages.tsx
+++ src/components/SlidingImages.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-
-// import React, { useState, useEffect } from "react";
 const slidingImages = [
   {
     id: 1,
